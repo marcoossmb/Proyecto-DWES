507794b5862e6666c8877e5d3c9df64c4e69419f
marcos <marcossblazquezz@gmail.com>
2023-11-08 19:18:29 +0100

    Inicialización del proyecto

---
+++ pages/calendario.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Proyecto Fútbol</title>
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    <body>
+        
+    </body>
+</html>
