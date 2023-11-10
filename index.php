d666a632838985fafe04b6f567c06ba33b2f6f33
ValentinoSanchez00 <sanchezravertavalentino@gmail.com>
2023-11-10 16:32:47 +0100

    Sing now created

--- index.php
+++ index.php
@@ -4,8 +4,38 @@
         <meta charset="UTF-8">
         <title>Proyecto Fútbol</title>
         <link rel="stylesheet" href="./css/style.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     </head>
-    <body>
-        
+    <body class="body">
+        <div class="contenedor">
+            <header class="header">
+                <h1 class="header__title">BIENVENIDO A LA PÁGINA DEL EQUIPO</h1>   
+            </header>
+            <main class="main">
+                <h2 class="main__title">
+                    INICIA SESION
+                </h2>
+                <form method="post" action="./pages/calendario.php">
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Usuario</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                       
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+              
+                    <button class="boton bg-success" type="submit" class="btn btn-primary">Entrar</button>
+                </form>
+
+            </main>
+
+
+
+
+
+        </div>  
+
     </body>
 </html>
