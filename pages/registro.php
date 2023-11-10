5312fe6152c1ec7fe86ac028497a77cc59faf908
marcos <marcossblazquezz@gmail.com>
2023-11-10 17:12:39 +0100

    registration page created

--- pages/registro.php
+++ pages/registro.php
@@ -3,9 +3,53 @@
     <head>
         <meta charset="UTF-8">
         <title>Proyecto Fútbol</title>
-        <link rel="stylesheet" href="../css/style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/registro.css">
     </head>
     <body>
-        
+        <div class="contenedor">
+            <main class="main">
+                <h1 class="main__h1">REGISTRATE</h1>
+                <form class="main__form">
+                    <div class="d-flex mb-3 mt-3">
+                        <div class="form-group right">
+                            <label class="form__label">Nombre</label>
+                        <input type="text" class="form-control" id="nombre" name="nombre" required>
+                      </div>
+                      <div class="form-group">
+                        <label class="form__label">Apellidos</label>
+                        <input type="text" class="form-control" id="apellidos" name="apellidos" required>
+                      </div>
+                    </div>
+                    
+                    <div class="d-flex mb-3 mt-3">
+                        <div class="form-group right">
+                          <label class="form__label">DNI:</label>
+                          <input type="text" class="form-control" id="dni" name="dni" required>
+                        </div>
+                        <div class="form-group mb-3">
+                          <label class="form__label">Dorsal:</label>
+                          <input type="text" class="form-control" id="dorsal" name="dorsal" required>
+                        </div>
+                    </div>
+                    
+                    <div class="d-flex mb-3 mt-3">
+                        <div class="form-group right">
+                          <label class="form__label">Usuario:</label>
+                          <input type="text" class="form-control" id="usuario" name="usuario" required>
+                        </div>                   
+                        <div class="form-group mb-3">
+                          <label class="form__label">Correo electrónico:</label>
+                          <input type="email" class="form-control" id="email" name="email" required>
+                        </div>
+                    </div>
+                    <div class="form-group mb-3">
+                      <label class="form__label">Contraseña:</label>
+                      <input type="password" class="form-control" id="contrasena" name="contrasena" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary bg-success form__button mb-3">Registrarse</button>
+                  </form>
+            </main>
+        </div>
     </body>
 </html>
