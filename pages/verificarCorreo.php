a349f3da85b676e78ba079cefdde571259c678d1
ValentinoSanchez00 <sanchezravertavalentino@gmail.com>
2023-11-28 19:08:33 +0100

    Interior del corre mejorado

--- pages/verificarCorreo.php
+++ pages/verificarCorreo.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="../css/correo.css"/>
 <?php
 
 use PHPMailer\PHPMailer\PHPMailer;
@@ -42,14 +44,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $mail->isHTML(true);
             $mail->CharSet = 'UTF-8';
             $mail->Subject = "Verificación de Cambio de Contraseña";
-            $mail->Body = "<h1>Estimado/a " . $nombre . "</h1><br><br>
-                           Hemos recibido una solicitud para cambiar la contraseña asociada a su cuenta en la página web de tu equipo.<br><br>
-                           Para garantizar la seguridad de su cuenta, necesitamos verificar que esta solicitud proviene legítimamente de usted. Por favor, haga clic en el siguiente enlace para confirmar su intención de cambiar la contraseña:<br><br>
-                           <a href='http://localhost/Proyecto-DWES/pages/cambiarcontra.php?verificarTrue&&nombre=".urlencode($nombre_codificado)."'>Verificar Correo</a><br><br>
-                           Si no ha solicitado este cambio o no reconoce esta actividad, le recomendamos que ignore este mensaje y tome las medidas necesarias para asegurar su cuenta.";
+            $mail->Body = "
+                            <!DOCTYPE html>
+                            <html lang='es'>
+                            <head>
+                                <meta charset='UTF-8'>
+                                <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+                            </head>
+                            <body style='font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px;'>
+                                <table role='presentation' cellspacing='0' cellpadding='0' width='100%' style='margin: auto;'>
+                                    <tr>
+                                        <td style='max-width: 600px; margin: 0 auto; background-color: #ffffff; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);'>
+                                            <h1 style='color: #333333; font-size: 24px; margin-bottom: 20px;'>Estimado/a " . $nombre . "</h1>
+                                            <p style='color: #555555; font-size: 16px; margin-bottom: 20px;'>Hemos recibido una solicitud para cambiar la contraseña asociada a su cuenta en la página web de tu equipo.</p>
+                                            <p style='color: #555555; font-size: 16px; margin-bottom: 20px;'>Para garantizar la seguridad de su cuenta, necesitamos verificar que esta solicitud proviene legítimamente de usted.</p>
+                                            <p style='color: #555555; font-size: 16px; margin-bottom: 20px;'>Por favor, haga clic en el siguiente enlace para confirmar su intención de cambiar la contraseña:</p>
+                                            <p style='margin-bottom: 30px;'><a href='http://localhost/Proyecto-DWES/pages/cambiarcontra.php?verificarTrue' style='color: #ffffff; text-decoration: none; font-size: 16px; display: inline-block; padding: 10px 20px; background-color: #28a745; border-radius: 5px; text-align: center;'>Verificar Correo</a></p>
+                                            <p style='color: #555555; font-size: 16px;'>Si no ha solicitado este cambio o no reconoce esta actividad, le recomendamos que ignore este mensaje y tome las medidas necesarias para asegurar su cuenta.</p>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </body>
+                            </html>";
+
             $mail->send();
+
             
             header("Location: ./cambiarContra.php?enviado&&nombre='.urlencode($nombre_codificado).'");
+
         } else {
             header("Location: ./cambiarContra.php?denegado");
         }
